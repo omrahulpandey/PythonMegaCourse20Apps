d748e35111de19c72983ad3a086795f6b496e3a3
omrahulpandey <omrahulpandey@yahoo.in>
2024-02-21 22:09:32 +0530

    Day2 completed

---
+++ Day2/bonus2.py
@@ -0,0 +1,5 @@
+user_prompt = "Enter your name: "
+
+while True:
+    name = input(user_prompt).capitalize()
+    print(name)
---
+++ Day2/main.py
@@ -0,0 +1,7 @@
+user_prompt = "Enter a ToDo: "
+
+todos = []
+while 2>1:
+    todo = input(user_prompt)
+    todos.append(todo.capitalize())
+    print(todos)
