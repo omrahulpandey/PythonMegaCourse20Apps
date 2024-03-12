642b26dda29d3385d0075b094a2431087741fe3b
omrahulpandey <omrahulpandey@yahoo.in>
2024-03-13 00:34:06 +0530

    Day13 updated

--- Day13/main.py
+++ Day13/main.py
@@ -27,7 +27,31 @@ while True:
         for index, item in enumerate(todos):
             print(f"{index+1}.{item.strip()}")
 
+    elif user_action.startswith('edit'):
+        try:
+            todo_index = int(user_action[5:].strip())
 
+            todos = get_todos(path)
+
+            todo = input("Enter the ToDo: ").strip().title()
+
+            todos[todo_index-1] = todo + '\n'
+
+            write_todos(path, todos)
+        except:
+            print("Invlaid input, try again.")
+
+    elif user_action.startswith('complete'):
+        try:
+            todo_index = int(user_action[8:].strip())
+
+            todos = get_todos(path)
+            todos.pop(todo_index-1)
+
+            write_todos(path, todos)
+        except:
+            print("Invaid input, try again.")
+            
     elif user_action.startswith('exit'):
         break
 
