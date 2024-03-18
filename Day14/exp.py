ea5848ba89abe3d694ad16503a2a892b69c14e5a
omrahulpandey <omrahulpandey@yahoo.in>
2024-03-18 22:22:57 +0530

    Day14 updated

---
+++ Day14/exp.py
@@ -0,0 +1,10 @@
+def parse(user_input):
+    feet = int(user_input[0])
+    inches = int(user_input[1])
+    return feet, inches
+
+def convert()
+
+user_input = input("Enter height in feet and inches: ").strip()
+
+print(convert)
--- Day14/functions.py
+++ Day14/functions.py
@@ -1,5 +1,5 @@
 # Functions
-def get_todos(path = "Day13/todos.txt"):
+def get_todos(path = "Day14/todos.txt"):
     """ Read a text file and returns list of todos 
     from the text file. 
     """
@@ -8,8 +8,11 @@ def get_todos(path = "Day13/todos.txt"):
         return todos_local
 
 
-def write_todos(todos_arg, path = "Day13/todos.txt"):
+def write_todos(todos_arg, path = "Day14/todos.txt"):
     """ write items in todos to the text file."""
     with open(path,'w') as file:
         file.writelines(todos_arg)
 
+print(__name__)
+if __name__ == 'main':
+    print(get_todos())
